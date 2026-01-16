a1178db231b70fe7a0775decad18f584cd57a463
Andrew <andrew.publik.mail@gmail.com>
2026-01-16 18:39:56 +0300

    refresh token

--- internal/http/router.go
+++ internal/http/router.go
@@ -32,7 +32,7 @@ func NewRouter(
 	r.Use(middleware.Recoverer)
 	r.Use(middleware.Timeout(30 * time.Second))
 	r.Use(cors.Handler(cors.Options{
-		AllowedOrigins:   []string{"*"},
+		AllowedOriginFunc: func(origin string) bool { return true },
 		AllowedMethods:   []string{"GET", "POST", "PUT", "DELETE", "OPTIONS"},
 		AllowedHeaders:   []string{"Accept", "Authorization", "Content-Type", "X-Requested-With"},
 		ExposedHeaders:   []string{"Link", "Set-Cookie"},
